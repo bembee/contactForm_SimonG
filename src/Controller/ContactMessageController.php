7df9d770527c787324725853d21bffc467d1ce05
bembee <bembee@freemail.hu>
2023-06-25 22:29:07 +0200

    make controller, route

---
+++ src/Controller/ContactMessageController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContactMessage;
+use App\Form\ContactMessageType;
+use App\Repository\ContactMessageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/contact')]
+class ContactMessageController extends AbstractController
+{
+    // #[Route('/', name: 'app_contact_message_index', methods: ['GET'])]
+    // public function index(ContactMessageRepository $contactMessageRepository): Response
+    // {
+    //     return $this->render('contact_message/index.html.twig', [
+    //         'contact_messages' => $contactMessageRepository->findAll(),
+    //     ]);
+    // }
+
+    #[Route('/', name: 'app_contact_message_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ContactMessageRepository $contactMessageRepository): Response
+    {
+        $contactMessage = new ContactMessage();
+        $form = $this->createForm(ContactMessageType::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactMessageRepository->save($contactMessage, true);
+
+            return $this->redirectToRoute('app_contact_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('contact_message/contact.html.twig', [
+            'contact_message' => $contactMessage,
+            'form' => $form,
+        ]);
+    }
+}
